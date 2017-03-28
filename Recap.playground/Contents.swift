252f8b2d74b3c76e5c19d72aa5dd8552da4d3d51
Russell Gordon <rgordon@rsgc.on.ca>
2017-03-28 08:24:42 -0400

    Initial page ready to go

---
+++ Recap.playground/Contents.swift
@@ -0,0 +1,11 @@
+/*:
+ # Recap
+ After a long and hopefully restful break, we'll take a class to look back on what you've learned so far, before digging in and learning new concepts.
+ 
+ Take today's class to add notes to your sketchbook (remember this can be brought into your final exam and used in your ISP video explanation as a reference):
+ 
+ ![learning_objectives](learning_objectives.png "Learning Objectives")
+ 
+ [Next](@next)
+ 
+ */
