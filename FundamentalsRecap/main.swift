36da4744459c59791f523af6bb14095dc3537741
Russell Gordon <rgordon@rsgc.on.ca>
2017-03-28 08:18:56 -0400

    Initial Commit

---
+++ FundamentalsRecap/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FundamentalsRecap
+//
+//  Created by Russell Gordon on 3/28/17.
+//  Copyright © 2017 Russell Gordon. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
