0c0c0ac5a362d31694f9d67933ea5ffe933dd306
Russell Gordon <rgordon@rsgc.on.ca>
2017-03-28 08:32:49 -0400

    Sequence page added

---
+++ Recap.playground/Pages/Sequence.xcplaygroundpage/Contents.swift
@@ -0,0 +1,35 @@
+/*:
+ [Previous](@previous) / [Next](@next)
+ 
+ # Sequence
+ 
+ Some new shape types and options have been added to the Canvas class.
+ 
+ Use this new functionality, and your [knowledge of colour](http://russellgordon.ca/rsgc/2016-17/ics2o/HSB_Colour_Model_-_Summary_-_Swift_3.pdf), to reproduce this abstract version of the eBay logo:
+ 
+ ![new_ebay_logo.png](new_ebay_logo.jpg "Overlapping Shapes Logo")
+ 
+ Color and alpha (transparency) matter.
+ 
+ Try reproducing this image for practice.
+ 
+ ## Remember
+ 
+ Commit your work as you make progress on this page.
+ 
+ ## Note
+ 
+ The following two statements are required to make the playground run. Please do not remove.
+ */
+import Cocoa
+import PlaygroundSupport
+
+// Create canvas
+let canvas = Canvas(width: 500, height: 400)
+
+
+/*:
+ ## Template code
+ The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
+ */
+PlaygroundPage.current.liveView = canvas.imageView
